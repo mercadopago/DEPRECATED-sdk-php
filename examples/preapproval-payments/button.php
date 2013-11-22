a25df63b6f8e31158e11bd2283f728d8601098a1
Matias Gordon <matias.gordon@mercadolibre.com>
2013-11-22 12:22:25 -0200

    Update button.php

--- examples/preapproval-payments/button.php
+++ examples/preapproval-payments/button.php
@@ -3,7 +3,7 @@ require_once "../../lib/mercadopago.php";
 
 $mp = new MP("CLIENT_ID", "CLIENT_SECRET");
 
-$preapprovalPayment = array(
+$preapprovalPayment_data = array(
     "payer_email" => "my_customer@my_site.com",
     "back_url" => "http://www.my_site.com",
     "reason" => "Monthly subscription to premium package",
@@ -18,7 +18,7 @@ $preapprovalPayment = array(
     )
 );
 
-$preapprovalPaymentResult = $mp->create_preapproval_payment($preapprovalPayment);
+$preapprovalPayment = $mp->create_preapproval_payment($preapprovalPayment_data);
 ?>
 
 <!doctype html>
@@ -27,7 +27,7 @@ $preapprovalPaymentResult = $mp->create_preapproval_payment($preapprovalPayment)
         <title>MercadoPago SDK - Create Preapproval Payment and Show Subscription Example</title>
     </head>
     <body>
-       	<a href="<?php echo $preapprovalPaymentResult["response"]["init_point"]; ?>" name="MP-Checkout" class="orange-ar-m-sq-arall">Pay</a>
+       	<a href="<?php echo $preapprovalPayment["response"]["init_point"]; ?>" name="MP-Checkout" class="orange-ar-m-sq-arall">Pay</a>
         <script type="text/javascript" src="http://mp-tools.mlstatic.com/buttons/render.js"></script>
     </body>
 </html>
